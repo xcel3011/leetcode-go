e376816ad5eecd20caccdfc57244717c2f1ec35b
shifeng <111>
2021-10-18 19:46:31 +0800

    141 环形链表（简单）

---
+++ LinkedList/hasCycle-141.go
@@ -0,0 +1,14 @@
+package LinkedList
+
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
