fa5eeb75ff50db9a82f8c87c5c7671c1f9e9b349
shifeng <111>
2022-01-04 19:46:56 +0800

    116 填充二叉树节点的右侧指针 中等

---
+++ tree/connect_116.go
@@ -0,0 +1,40 @@
+package tree
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	connectTwoNode(root.Left, root.Right)
+
+	return root
+}
+
+func connectTwoNode(left, right *Node) {
+	if left == nil || right == nil {
+		return
+	}
+
+	left.Next = right
+
+	connectTwoNode(left.Left, left.Right)
+	connectTwoNode(right.Left, right.Right)
+	connectTwoNode(left.Right, right.Left)
+}
