ca4e7814c736f64e6f0a36c6c7fbd1d649577873
shifeng <111>
2022-01-17 15:52:35 +0800

    102 层序遍历 简单

---
+++ tree/levelOrder_102.go
@@ -0,0 +1,42 @@
+package tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	var (
+		queue   []*TreeNode
+		results [][]int
+	)
+
+	queue = append(queue, root)
+	for len(queue) != 0 {
+		size := len(queue)
+		var result []int
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			result = append(result, node.Val)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		results = append(results, result)
+	}
+
+	return results
+}
