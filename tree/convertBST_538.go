c5a379d1a416229d24181e1530c67d4f880c5120
shifeng <275517800@qq.com>
2022-01-24 10:56:10 +0800

    538 二叉搜索树转换为累加树 中等

---
+++ tree/convertBST_538.go
@@ -0,0 +1,26 @@
+package tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	var traverse func(*TreeNode)
+	traverse = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		traverse(node.Right)
+		sum += node.Val
+		node.Val = sum
+		traverse(node.Left)
+	}
+	traverse(root)
+	return root
+}
