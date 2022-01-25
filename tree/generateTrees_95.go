af1b7b241d93d8a4d6549460958516e207c49c42
shifeng <275517800@qq.com>
2022-01-25 15:23:43 +0800

    92 不同的二叉搜索树2 中等

---
+++ tree/generateTrees_95.go
@@ -0,0 +1,35 @@
+package tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+	var helper func(lo, hi int) []*TreeNode
+	helper = func(lo, hi int) []*TreeNode {
+		var res []*TreeNode
+		if lo > hi {
+			return append(res, nil)
+		}
+
+		for i := lo; i <= hi; i++ {
+			left := helper(lo, i-1)
+			right := helper(i+1, hi)
+			for _, leftNode := range left {
+				for _, rightNode := range right {
+					root := &TreeNode{Val: i}
+					root.Left = leftNode
+					root.Right = rightNode
+					res = append(res, root)
+				}
+			}
+		}
+		return res
+	}
+
+	return helper(1, n)
+}
