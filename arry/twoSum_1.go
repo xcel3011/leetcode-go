2e7a69f5b968528a543de57222fc0d0616cc3c48
shifeng <275517800@qq.com>
2022-02-09 16:49:02 +0800

    1 两数之和 简单

---
+++ arry/twoSum_1.go
@@ -0,0 +1,18 @@
+package arry
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		m[num] = i
+	}
+
+	for i, num := range nums {
+		other := target - num
+		index, ok := m[other]
+		if ok && i != index {
+			return []int{i, index}
+		}
+	}
+
+	return nil
+}
