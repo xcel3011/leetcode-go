8019b61e4eb73c66de8f419357e2aba9c77446a2
shifeng <275517800@qq.com>
2022-02-14 10:06:08 +0800

    26 删除数组中的重复项 简单

---
+++ array/removeDuplicates_26.go
@@ -0,0 +1,13 @@
+package array
+
+func removeDuplicates(nums []int) int {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
+}
