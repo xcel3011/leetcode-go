2fd248c0b34937d5fa4100485bcff6a395de173e
shifeng <275517800@qq.com>
2022-02-14 10:17:55 +0800

    83 删除链表中的重复项 简单

---
+++ linkedlist/deleteDuplicates_83.go
@@ -0,0 +1,22 @@
+package linkedlist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		if fast.Val != slow.Val {
+			slow.Next = fast
+			slow = slow.Next
+		}
+		fast = fast.Next
+	}
+
+	slow.Next = nil
+	return head
+}
