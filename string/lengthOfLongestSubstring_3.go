5931165f3203c9461576bb9c97f1d578118c2942
shifeng <275517800@qq.com>
2022-02-15 13:52:50 +0800

    3 无重复字符的最长字串 中等

---
+++ string/lengthOfLongestSubstring_3.go
@@ -0,0 +1,27 @@
+package string
+
+func lengthOfLongestSubstring(s string) int {
+	window := make(map[byte]int)
+
+	left, right := 0, 0
+	res := 0
+
+	for right < len(s) {
+		c := s[right]
+		right++
+		window[c]++
+
+		for window[c] > 1 {
+			d := s[left]
+			left++
+			window[d]--
+		}
+
+		diff := right - left
+		if res < diff {
+			res = diff
+		}
+	}
+
+	return res
+}
