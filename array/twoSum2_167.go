d3b12a2802309acdbebdc786a9ab3d4b59ef383e
shifeng <275517800@qq.com>
2022-02-15 10:58:06 +0800

    167 两数之和2 中等

---
+++ array/twoSum2_167.go
@@ -0,0 +1,16 @@
+package array
+
+func twoSum2(numbers []int, target int) []int {
+	lo, hi := 0, len(numbers)-1
+	for hi > lo {
+		sum := numbers[lo] + numbers[hi]
+		if sum > target {
+			hi--
+		} else if sum < target {
+			lo++
+		} else {
+			return []int{lo + 1, hi + 1}
+		}
+	}
+	return nil
+}
