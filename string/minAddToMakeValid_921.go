738ddf3d83461809aae33ce8efb9059070f3ce26
shifeng <275517800@qq.com>
2022-02-21 13:11:34 +0800

    921 使括号中有效添加最少 中等

---
+++ string/minAddToMakeValid_921.go
@@ -0,0 +1,20 @@
+package string
+
+func minAddToMakeValid(s string) int {
+	var res []byte
+	of := func(c byte) byte {
+		if c == '(' {
+			return ' '
+		}
+		return '('
+	}
+
+	for i := range s {
+		if len(res) != 0 && res[len(res)-1] == of(s[i]) {
+			res = res[:len(res)-1]
+		} else {
+			res = append(res, s[i])
+		}
+	}
+	return len(res)
+}
