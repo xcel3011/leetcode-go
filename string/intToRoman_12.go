fc9d62bf3d85e645b8bc535b4a2bef266706b113
shifeng <275517800@qq.com>
2022-02-21 17:20:49 +0800

    12 整数转罗马数 简单

--- string/intToRoman_12.go
+++ string/intToRoman_12.go
@@ -1,18 +1,11 @@
 package string
 
 func intToRoman(num int) string {
-	m := map[int]string{1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
-	s := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	res := ""
+	// 1 <= num <= 3999
+	I := [10]string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+	X := [10]string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	C := [10]string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	M := [4]string{"", "M", "MM", "MMM"}
 
-	for _, v := range s {
-		for num >= v {
-			num -= v
-			res += m[v]
-		}
-		if num == 0 {
-			break
-		}
-	}
-	return res
+	return M[num/1000] + C[num%1000/100] + X[num%100/10] + I[num%10]
 }
