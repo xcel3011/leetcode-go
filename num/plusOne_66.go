8c55fb8a6e6044061729b33700fc9689964b4aaa
shifeng <275517800@qq.com>
2022-02-21 18:51:41 +0800

    66 加一 简单

---
+++ num/plusOne_66.go
@@ -0,0 +1,24 @@
+package num
+
+func plusOne(digits []int) []int {
+	carry := 0
+	n := 0
+	l := len(digits) - 1
+	var res []int
+	for ; l >= 0; l-- {
+		if l == len(digits)-1 {
+			n = digits[l] + 1 + carry
+		} else {
+			n = digits[l] + carry
+		}
+
+		carry = n / 10
+		n = n % 10
+		res = append([]int{n}, res...)
+	}
+
+	if carry != 0 {
+		res = append([]int{carry}, res...)
+	}
+	return res
+}
