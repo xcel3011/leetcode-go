90e0b2740483e5232e919c1a55f9d1df8c6263fe
shifeng <275517800@qq.com>
2022-11-11 09:34:23 +0800

    1704 判断字符串的两半是否相似 简单

---
+++ string/halvesAreAlike_1704_easy.go
@@ -0,0 +1,20 @@
+package string
+
+func halvesAreAlike(s string) bool {
+	n := len(s)
+	mid := n / 2
+	s1, s2 := s[:mid], s[mid:]
+	count1, count2 := 0, 0
+	m := map[int32]struct{}{97: {}, 101: {}, 105: {}, 111: {}, 117: {}, 65: {}, 69: {}, 73: {}, 79: {}, 85: {}}
+	for _, b := range s1 {
+		if _, ok := m[b]; ok {
+			count1++
+		}
+	}
+	for _, b := range s2 {
+		if _, ok := m[b]; ok {
+			count2++
+		}
+	}
+	return count1 == count2
+}
