518e4c73d286acca0286a2f50623c60733e3f3af
shifeng <275517800@qq.com>
2022-11-15 10:59:19 +0800

    791 自定义字符串排序 中等

---
+++ string/customSortString_791_mid.go
@@ -0,0 +1,13 @@
+package string
+
+import "sort"
+
+func customSortString(order string, s string) string {
+	m := make(map[byte]int)
+	for i := 0; i < len(order); i++ {
+		m[order[i]] = i + 1
+	}
+	t := []byte(s)
+	sort.Slice(t, func(i, j int) bool { return m[t[i]] < m[t[j]] })
+	return string(t)
+}
