66147320075ca9449ec4855795da1ae8b6947618
shifeng <275517800@qq.com>
2022-11-15 18:54:29 +0800

    518 零钱兑换2 中等

---
+++ dynamic/change_518_mid.go
@@ -0,0 +1,21 @@
+package dynamic
+
+func change2(amount int, coins []int) int {
+	n := len(coins)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, amount+1)
+		dp[i][0] = 1
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= amount; j++ {
+			if j-coins[i-1] >= 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+	return dp[n][amount]
+}
