c60a5d5171b7fa6c65df3750b7c526f284739179
shifeng <275517800@qq.com>
2022-11-16 11:14:55 +0800

    775 全局倒置与局部倒置 中等

---
+++ array/isIdealPermutation_775_mid.go
@@ -0,0 +1,17 @@
+package array
+
+// 全局倒置 			nums[i]>nums[j]	i<j
+// 局部倒置 			nums[i]>nums[j]	i=j-1
+// 全局倒置且不是局部	nums[i]>nums[j]	i<j-1
+func isIdealPermutation(nums []int) bool {
+	max := 0
+	for i := 2; i < len(nums); i++ {
+		if nums[i-2] > max {
+			max = nums[i-2]
+		}
+		if nums[i] > max {
+			return false
+		}
+	}
+	return true
+}
