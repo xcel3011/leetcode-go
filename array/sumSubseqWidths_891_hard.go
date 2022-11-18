6c6e9cee926950b348b65219538ac640cc50f9f3
shifeng <275517800@qq.com>
2022-11-18 14:08:18 +0800

    891 子序列宽度之和 困难

---
+++ array/sumSubseqWidths_891_hard.go
@@ -0,0 +1,37 @@
+package array
+
+import "sort"
+
+// 超出时间限制
+func sumSubseqWidths(nums []int) int {
+	var subsets [][]int
+	subsets = append(subsets, []int{})
+	for _, num := range nums {
+		subsetsLen := len(subsets)
+		for i := 0; i < subsetsLen; i++ {
+			subsets = append(subsets, append([]int{}, append(subsets[i], num)...))
+		}
+	}
+
+	var ans int
+	for _, subset := range subsets {
+		if len(subset) < 2 {
+			continue
+		}
+		sort.Ints(subset)
+		ans += subset[len(subset)-1] - subset[0]
+	}
+	return ans
+}
+
+func sumSubseqWidthsMath(nums []int) int {
+	const mod int = 1e9 + 7
+	sort.Ints(nums)
+	p, n := 1, len(nums)
+	ans := 0
+	for i, num := range nums {
+		ans = (ans + (num-nums[n-i-1])*p + mod) & mod
+		p = (p << 1) % mod
+	}
+	return ans
+}
