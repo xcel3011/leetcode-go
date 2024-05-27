17f5324e2b75a368e78ba3400286900f19f8b10e
shifeng <275517800@qq.com>
2024-05-27 10:46:13 +0800

    2006 差的绝对值为K的数对数目 简单

---
+++ array/2006_countKDifference_easy.go
@@ -0,0 +1,13 @@
+package array
+
+func countKDifference(nums []int, k int) int {
+	var count int
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]-nums[j] == k || nums[j]-nums[i] == k {
+				count++
+			}
+		}
+	}
+	return count
+}
