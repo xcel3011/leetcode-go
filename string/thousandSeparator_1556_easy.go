8bff61c922defd912a52fc472dd1dd0ca618d7be
shifeng <275517800@qq.com>
2024-05-28 11:27:58 +0800

    1556 前分位分隔数 简单

---
+++ string/thousandSeparator_1556_easy.go
@@ -0,0 +1,17 @@
+package string
+
+import (
+	"strconv"
+)
+
+func thousandSeparator(n int) string {
+	return solve(strconv.Itoa(n))
+}
+
+func solve(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return solve(s[:n-3]) + "." + s[n-3:]
+}
