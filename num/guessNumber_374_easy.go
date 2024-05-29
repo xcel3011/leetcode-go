c27d2daa0425d83f794eeaacb4ea71a0e167d016
shifeng <275517800@qq.com>
2024-05-29 15:27:42 +0800

    374 猜数字大小 简单

---
+++ num/guessNumber_374_easy.go
@@ -0,0 +1,18 @@
+package num
+
+func guess(num int) int {
+	return num
+}
+
+func guessNumber(n int) int {
+	left, right := 0, n
+	for left < right {
+		mid := left + (right-left)/2
+		if guess(mid) <= 0 {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
