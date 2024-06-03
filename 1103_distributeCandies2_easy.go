2962a50d2198294e76620ef649d88cbd1d2163de
shifeng <275517800@qq.com>
2024-06-03 10:17:35 +0800

    每日一题

--- 1103_distributeCandies2_easy.go
+++ 1103_distributeCandies2_easy.go
@@ -1,25 +1,11 @@
 package leetcode_go
 
-import (
-	"fmt"
-)
-
 func distributeCandies(candies int, num_people int) []int {
 	ans := make([]int, num_people)
 	n := 0
 	for candies > 0 {
-		for i := 0; i < num_people; i++ {
-			count := n*num_people + i
-			if count > candies {
-				count = candies
-			}
-			candies -= count
-			fmt.Println(count, candies)
-			ans[i] += count
-			if candies == 0 {
-				break
-			}
-		}
+		ans[n%num_people] += min(candies, n+1)
+		candies -= min(candies, n+1)
 		n++
 	}
 	return ans
