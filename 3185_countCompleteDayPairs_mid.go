cd88d53b296ffcee1997e2ec68a6eb4c5f69b21a
shifeng <275517800@qq.com>
2024-10-23 17:31:37 +0800

    3185. 构成整天的下标对数目 II

---
+++ 3185_countCompleteDayPairs_mid.go
@@ -0,0 +1,11 @@
+package leetcode_go
+
+func countCompleteDayPairs2(hours []int) int64 {
+	cnt := [24]int{}
+	var ans int64
+	for _, h := range hours {
+		ans += int64(cnt[24-h%24])
+		cnt[h%24]++
+	}
+	return ans
+}
