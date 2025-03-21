c276d7ae8a08544432dac498c2bf6d90000de17e
shifeng <shifeng@huiyan-tech.com>
2025-03-21 14:20:21 +0800

    二分查找匹配元素第一个和最后一个位置

--- 34_searchRange_mid.go
+++ 34_searchRange_mid.go
@@ -1,5 +1,9 @@
 package leetcode_go
 
+import (
+	"slices"
+)
+
 func searchRange(nums []int, target int) []int {
 	f := func(nums []int, target int) int {
 		left, right := 0, len(nums) // 左闭右开区间 [left, right)
@@ -23,4 +27,18 @@ func searchRange(nums []int, target int) []int {
 	// 如果 start 存在，那么 end 必定存在
 	end := f(nums, target+1) - 1
 	return []int{start, end}
+}
+
+// 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+//
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+//
+// 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+func searchRange250321(nums []int, target int) []int {
+	start, ok := slices.BinarySearch(nums, target)
+	if !ok {
+		return []int{-1, -1}
+	}
+	end, _ := slices.BinarySearch(nums, target+1)
+	return []int{start, end - 1}
 }
