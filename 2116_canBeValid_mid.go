266c5ca59278914f342fe9a35a9245edc6606d18
shifeng <shifeng@huiyan-tech.com>
2025-03-24 20:17:27 +0800

    250323每日一题

--- 2116_canBeValid_mid.go
+++ 2116_canBeValid_mid.go
@@ -12,4 +12,28 @@ package leetcode_go
 //如果你可以将 s 变为有效括号字符串，请你返回 true ，否则返回 false 。
 
 func canBeValid(s string, locked string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	mn, mx := 0, 0
+	for i, lock := range locked {
+		if lock == '1' {
+			// 左括号是 1，右括号是 -1
+			d := 1 - int(s[i]%2*2)
+			mx += d
+			if mx < 0 {
+				return false
+			}
+			mn += d
+		} else {
+			mx++ // 改成左括号，c 加一
+			mn-- // 改成右括号，c 减一
+		}
+		if mn < 0 { // c 不能为负
+			mn = 1 // 此时 c 的取值范围都是奇数，最小的奇数是 1
+		}
+
+	}
+	return mn == 0
 }
