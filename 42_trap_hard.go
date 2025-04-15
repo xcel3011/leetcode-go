a085b7d42a7efeda2a362db706b06a4c7951faa4
shifeng <275517800@qq.com>
2025-04-15 19:02:44 +0800

    接雨水

--- 42_trap_hard.go
+++ 42_trap_hard.go
@@ -17,3 +17,24 @@ func trap(height []int) int {
 	}
 	return ans
 }
+
+// 时间复杂的度：O(n)，其中 n 是数组 height 的长度。
+// 空间复杂度：O(n)，其中 n 是数组 height 的长度。空间复杂度主要取决于两个数组 preMax 和 sufMax 的长度。
+func trap250415(height []int) int {
+	n := len(height)
+	preMax := make([]int, n)
+	preMax[0] = height[0]
+	for i := 1; i < n; i++ {
+		preMax[i] = max(preMax[i-1], height[i])
+	}
+	sufMax := make([]int, n)
+	sufMax[n-1] = height[n-1]
+	for i := n - 2; i >= 0; i-- {
+		sufMax[i] = max(sufMax[i+1], height[i])
+	}
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans += min(preMax[i], sufMax[i]) - height[i]
+	}
+	return ans
+}
